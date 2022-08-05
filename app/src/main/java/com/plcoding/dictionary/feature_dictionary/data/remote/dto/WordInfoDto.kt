4c5db823aae465080c6887752e88be37064bef92
Maxmarra <5919669@gmail.com>
2022-08-05 10:12:19 +0300

    02_REPO

--- app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/WordInfoDto.kt
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/remote/dto/WordInfoDto.kt
@@ -1,5 +1,6 @@
 package com.plcoding.dictionary.feature_dictionary.data.remote.dto
 
+import com.plcoding.dictionary.feature_dictionary.data.local.entity.WordInfoEntity
 import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
 
 data class WordInfoDto(
@@ -9,8 +10,8 @@ data class WordInfoDto(
     val phonetics: List<PhoneticDto>,
     val word: String
 ) {
-    fun toWordInfoEntity(): WordInfo {
-        return WordInfo(
+    fun toWordInfoEntity(): WordInfoEntity {
+        return WordInfoEntity (
             meanings = meanings.map { it.toMeaning() },
             origin = origin,
             phonetic = phonetic,
