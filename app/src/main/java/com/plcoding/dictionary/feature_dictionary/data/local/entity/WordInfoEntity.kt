baa32513593f23cfb56976c3a85f69177f8c4d82
Maxmarra <5919669@gmail.com>
2022-08-05 09:38:08 +0300

    02_DB

---
+++ app/src/main/java/com/plcoding/dictionary/feature_dictionary/data/local/entity/WordInfoEntity.kt
@@ -0,0 +1,30 @@
+package com.plcoding.dictionary.feature_dictionary.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.plcoding.dictionary.feature_dictionary.domain.model.Meaning
+import com.plcoding.dictionary.feature_dictionary.domain.model.WordInfo
+
+@Entity
+data class WordInfoEntity(
+    val word: String,
+    val phonetic: String,
+    val origin: String,
+    //из-за этого списка мы создаем кучу файлов
+    //по хорошему для него нужно делать отдельную таблицу
+    //но можно сделать просто конвертирование
+    // data-util - JsonParser, GsonParser
+    // local - Converters
+    // обрати внимание на аннотации! (в WordInfoDatabase тоже нужна!!!)
+    val meanings: List<Meaning>,
+    @PrimaryKey val id: Int? = null
+) {
+    fun toWordInfo(): WordInfo {
+        return WordInfo(
+            meanings = meanings,
+            word = word,
+            origin = origin,
+            phonetic = phonetic
+        )
+    }
+}
